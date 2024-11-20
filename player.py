0b66f97bac8ca2f817718e69dca55f4f26d779d3
Bobby Ta <vhungta2006@gmail.com>
2024-11-20 23:00:24 +0000

    bumpiness -0.39 max_height -0.02

--- player.py
+++ player.py
@@ -180,7 +180,7 @@ class EpicPlayer(Player):
                 avg_height_weight = -0.10
                 max_height_weight = -0.02
                 lines_cleared_weight = 0
-                bumpiness_weight = -0.4
+                bumpiness_weight = -0.39
                 holes_weight = -2.55
                 cont_horizontal_weight = -0.2
                 cont_vertical_weight = -0.3
